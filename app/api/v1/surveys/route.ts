7be645776e55cf2f1f350d7f4d607f25fd8455e3
Cian Dolphin-Murray <dolphinmurraycian@gmail.com>
2023-11-11 20:53:37 +0800

    fix survey route

--- app/api/v1/surveys/route.ts
+++ app/api/v1/surveys/route.ts
@@ -1,27 +1,8 @@
-import { Prisma } from "@prisma/client";
-import { NextRequest, NextResponse } from "next/server";
-import prisma from "@/prisma/client";
-
-export type FullSurvey = Prisma.SurveyGetPayload<{
-  include: {
-    questions: {
-      include: {
-        answers: true;
-      };
-    };
-  };
-}>;
+import {NextRequest, NextResponse} from 'next/server';
+import prisma from '@/prisma/client';
 
 export async function GET(req: NextRequest, res: NextResponse) {
-  const surveys = await prisma.survey.findMany({
-    include: {
-      questions: {
-        include: {
-          answers: true,
-        },
-      },
-    },
-  });
+  const surveys = await prisma.survey.findMany({});
 
   return new Response(JSON.stringify(surveys));
 }
