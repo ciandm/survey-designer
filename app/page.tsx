b75a3ce62c9b89c0126c640cf02b29458d2e3757
Cian Dolphin-Murray <dolphinmurraycian@gmail.com>
2024-02-26 18:01:26 +1000

    feat: update survey card styles

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,65 @@
+import {Button} from '@/components/ui/button';
+import {Separator} from '@/components/ui/separator';
 import {SurveyCard} from '@/features/home/components/survey-card';
-import {SurveyCardActions} from '@/features/home/components/survey-card-actions';
-import {surveySchema} from '@/lib/validations/survey';
+import {
+  SurveyResponse,
+  SurveySchema,
+  surveySchema,
+} from '@/lib/validations/survey';
 import prisma from '@/prisma/client';
 
+type SurveysWithResponseCount = SurveyResponse['survey'] & {
+  responseCount: number;
+};
+
+async function getHomeSurveys(): Promise<SurveysWithResponseCount[]> {
+  const surveys = await prisma.survey.findMany({
+    include: {
+      SurveyResult: true,
+    },
+  });
+
+  return surveys
+    .map((survey) => {
+      const parsedSchema = surveySchema.safeParse(survey.schema);
+      if (!parsedSchema.success) {
+        return null;
+      }
+
+      return {
+        ...survey,
+        responseCount: survey.SurveyResult.length,
+        schema: parsedSchema.data,
+      };
+    })
+    .filter(validateIsNotNull);
+}
+
+function validateIsNotNull<T>(value: T | null): value is T {
+  return value !== null;
+}
+
 const Home = async () => {
-  const surveys = await prisma.survey.findMany();
+  const surveys = await getHomeSurveys();
 
   return (
     <div className="container p-4">
-      <h2 className="mb-6 mt-10 scroll-m-20 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
-        Surveys
-      </h2>
-      <ul className="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-4">
+      <div className="flex items-center justify-between">
+        <h1 className="text-xl font-semibold tracking-tight transition-colors first:mt-0">
+          Surveys
+        </h1>
+        <Button>Create Survey</Button>
+      </div>
+      <Separator className="mb-6 mt-4" />
+      <ul className="flex flex-col gap-4 sm:grid sm:grid-cols-[repeat(auto-fit,minmax(480px,1fr))] md:gap-4">
         {surveys.map((survey) => {
-          const parsedSchema = surveySchema.safeParse(survey.schema);
-          if (!parsedSchema.success) {
-            return null;
-          }
-          const {data} = parsedSchema;
-
           return (
-            <SurveyCard schema={data} key={survey.id}>
-              <SurveyCardActions surveyId={survey.id} />
-            </SurveyCard>
+            <SurveyCard
+              responsesCount={survey.responseCount}
+              schema={survey.schema}
+              survey={survey}
+              key={survey.id}
+            />
           );
         })}
       </ul>
--- features/home/components/survey-card.tsx
+++ features/home/components/survey-card.tsx
@@ -1,27 +1,91 @@
+'use client';
+
+import {DotsHorizontalIcon} from '@radix-ui/react-icons';
+import Link from 'next/link';
+import {Button} from '@/components/ui/button';
+import {Card, CardFooter, CardHeader, CardTitle} from '@/components/ui/card';
 import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card';
-import {SurveySchema} from '@/lib/validations/survey';
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
+import {Separator} from '@/components/ui/separator';
+import {cn} from '@/lib/utils';
+import {SurveyResponse, SurveySchema} from '@/lib/validations/survey';
 
 type SurveyCardProps = {
   schema: SurveySchema;
-  children: React.ReactNode;
+  survey: SurveyResponse['survey'];
+  responsesCount: number;
 };
 
-export const SurveyCard = ({schema, children}: SurveyCardProps) => {
+export const SurveyCard = ({
+  schema,
+  survey,
+  responsesCount,
+}: SurveyCardProps) => {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>{schema.title}</CardTitle>
-        <CardDescription>{schema.description}</CardDescription>
-      </CardHeader>
-      <CardContent>{schema.elements.length} questions</CardContent>
-      <CardFooter className="justify-between">{children}</CardFooter>
-    </Card>
+    <DropdownMenu key={survey.id}>
+      <Card className="p-0">
+        <CardHeader className="bg-muted p-6">
+          <div className="flex items-center justify-between">
+            <div className="space-y-1">
+              <div className="flex items-center gap-2">
+                <div
+                  className={cn(
+                    'flex h-4 w-4 items-center justify-center rounded-full bg-muted',
+                    {'bg-green-500/20': survey.is_published},
+                  )}
+                >
+                  <div
+                    className={cn('h-2 w-2 rounded-full bg-muted-foreground', {
+                      'bg-green-500': survey.is_published,
+                    })}
+                  />
+                </div>
+                <CardTitle className="text-base">{schema.title}</CardTitle>
+              </div>
+            </div>
+            <div className="flex space-x-2">
+              <Button variant="outline" className="hidden sm:flex" asChild>
+                <Link href={`/editor/${survey.id}/designer`}>Edit</Link>
+              </Button>
+              <DropdownMenuTrigger asChild>
+                <Button size="icon" variant="ghost">
+                  <DotsHorizontalIcon />
+                </Button>
+              </DropdownMenuTrigger>
+            </div>
+          </div>
+        </CardHeader>
+        <CardFooter className="justify-between px-6 py-4">
+          <div className="flex w-full flex-col gap-4">
+            <div className="flex justify-between">
+              <p className="text-sm text-muted-foreground">Responses</p>
+              <p className="text-sm text-muted-foreground">{responsesCount}</p>
+            </div>
+            <Separator />
+            <div className="flex justify-between">
+              <p className="text-sm text-muted-foreground">Last updated</p>
+              <p className="text-sm text-muted-foreground">
+                {new Date(survey.updatedAt).toLocaleDateString('en-GB')}
+              </p>
+            </div>
+          </div>
+        </CardFooter>
+      </Card>
+      <DropdownMenuContent className="w-[240px]">
+        <DropdownMenuLabel>{schema.title}</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Edit survey</DropdownMenuItem>
+        <DropdownMenuItem>Preview survey</DropdownMenuItem>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem>Make a copy</DropdownMenuItem>
+        <DropdownMenuItem>Delete survey</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 };
