ed664cfe1428c909f5f33a82fa7be6eec0433028
Cian Dolphin-Murray <dolphinmurraycian@gmail.com>
2024-03-01 21:55:55 +1000

    remove production blocker

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,47 +28,27 @@ export default async function RootLayout({
 }) {
   const {user} = await getUser();
 
-  let content = (
+  return (
     <SessionProvider value={{user}}>
       <QueryClientProvider>
         <LoadingOverlay>
           <DuplicateSurveyDialog>
-            <DeleteSurveyDialog>{children}</DeleteSurveyDialog>
+            <DeleteSurveyDialog>
+              <html lang="en">
+                <body
+                  className={cn(
+                    'bg-background font-sans antialiased',
+                    inter.variable,
+                  )}
+                >
+                  {children}
+                </body>
+              </html>
+            </DeleteSurveyDialog>
           </DuplicateSurveyDialog>
         </LoadingOverlay>
       </QueryClientProvider>
       <Toaster />
     </SessionProvider>
-  );
-
-  if (process.env.NODE_ENV === 'production') {
-    content = (
-      <div className="flex h-screen items-center justify-center">
-        <div className="flex max-w-sm flex-col gap-2">
-          <h1 className="text-lg font-medium">Work in progress</h1>
-          <p className="text-sm text-gray-500">
-            This app is currently under construction. In the meantime, you can
-            check out the code{' '}
-            <a
-              className="text-blue-500 underline underline-offset-2"
-              href="https://github.com/ciandm/survey-designer"
-            >
-              here
-            </a>{' '}
-            and make changes locally and connect to your own database.
-          </p>
-        </div>
-      </div>
-    );
-  }
-
-  return (
-    <html lang="en">
-      <body
-        className={cn('bg-background font-sans antialiased', inter.variable)}
-      >
-        {content}
-      </body>
-    </html>
   );
 }
