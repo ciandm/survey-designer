4939ccc3fb617e702776befa0ef7fc247133dfcb
Cian Dolphin-Murray <dolphinmurraycian@gmail.com>
2024-03-05 16:31:22 +1100

    add question-field and type-field

---
+++ components/question-field.tsx
@@ -0,0 +1,48 @@
+import {ErrorMessage} from '@hookform/error-message';
+import {useSurveyFormContext} from 'hooks/use-survey-form';
+import {cn} from '@/lib/utils';
+import {ElementSchemaType} from '@/types/element';
+import {FormDescription, FormField, FormItem, FormLabel} from './ui/form';
+import {TypeInputField} from './type-field';
+
+type QuestionFieldProps = {
+  index: number;
+  element: ElementSchemaType;
+};
+
+export const QuestionField = ({index, element}: QuestionFieldProps) => {
+  const {control} = useSurveyFormContext();
+  return (
+    <FormField
+      control={control}
+      name={`fields.${index}.value`}
+      render={({field}) => (
+        <FormItem>
+          <div className="flex flex-col gap-1">
+            <FormLabel
+              className={cn('break-normal text-base font-medium leading-6', {
+                [`after:content-['_*']`]: element.validations.required,
+              })}
+            >
+              {index + 1}. {!!element.text ? element.text : 'Untitled question'}
+            </FormLabel>
+            {!!element.description && (
+              <FormDescription>{element.description}</FormDescription>
+            )}
+          </div>
+          <div className="mt-4">
+            <TypeInputField field={field} element={element} />
+          </div>
+          <ErrorMessage
+            name={`fields.${index}.value`}
+            render={({message}) => (
+              <p className="text-sm font-medium leading-5 text-red-500">
+                {message}
+              </p>
+            )}
+          />
+        </FormItem>
+      )}
+    />
+  );
+};
---
+++ components/type-field.tsx
@@ -0,0 +1,122 @@
+import {ControllerRenderProps, useFormContext} from 'react-hook-form';
+import {SurveyFormState} from 'hooks/use-survey-form';
+import {Checkbox} from '@/components/ui/checkbox';
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+} from '@/components/ui/form';
+import {Input} from '@/components/ui/input';
+import {RadioGroup, RadioGroupItem} from '@/components/ui/radio-group';
+import {Textarea} from '@/components/ui/textarea';
+import {ElementSchemaType} from '@/types/element';
+
+type FieldProps = {
+  field: ControllerRenderProps<SurveyFormState, `fields.${number}.value`>;
+  element: ElementSchemaType;
+};
+
+export const TypeInputField = ({field, element}: FieldProps) => {
+  switch (element.type) {
+    case 'short_text':
+    case 'long_text':
+      return <TextField field={field} element={element} />;
+    case 'multiple_choice':
+      return <MultipleChoiceField field={field} element={element} index={0} />;
+    case 'single_choice':
+      return <SingleChoiceField field={field} element={element} />;
+    default:
+      return null;
+  }
+};
+
+const TextField = ({field, element}: FieldProps) => {
+  const Component = element.type === 'short_text' ? Input : Textarea;
+  return (
+    <FormControl>
+      <Component
+        {...field}
+        placeholder={element.properties.placeholder ?? 'Your answer here...'}
+        onChange={(e) => field.onChange([e.target.value])}
+      />
+    </FormControl>
+  );
+};
+
+type MultipleChoiceFieldProps = {
+  element: ElementSchemaType;
+  field: ControllerRenderProps<SurveyFormState, `fields.${number}.value`>;
+  index: number;
+};
+
+const MultipleChoiceField = ({element, index}: MultipleChoiceFieldProps) => {
+  const {control} = useFormContext<SurveyFormState>();
+  const choices = element.properties.choices ?? [];
+
+  return (
+    <div className="flex flex-col gap-2">
+      {choices.map((choice) => (
+        <FormField
+          key={choice.id}
+          control={control}
+          name={`fields.${index}.value`}
+          render={({field}) => (
+            <FormItem className="flex items-center space-x-2 space-y-0">
+              <FormControl>
+                <Checkbox
+                  {...field}
+                  checked={field.value.includes(choice.id)}
+                  onCheckedChange={(checked) => {
+                    if (checked) {
+                      field.onChange([...field.value, choice.id]);
+                    } else {
+                      field.onChange(
+                        field.value.filter((value) => value !== choice.id),
+                      );
+                    }
+                  }}
+                />
+              </FormControl>
+              <FormLabel className="flex items-center gap-x-3 text-sm font-medium">
+                {choice.value || '...'}
+              </FormLabel>
+            </FormItem>
+          )}
+        />
+      ))}
+    </div>
+  );
+};
+
+const SingleChoiceField = ({
+  element,
+  field,
+}: Omit<MultipleChoiceFieldProps, 'index'>) => {
+  const choices = element.properties.choices ?? [];
+
+  return (
+    <FormItem className="flex flex-col gap-2">
+      <FormControl>
+        <RadioGroup
+          onValueChange={(value) => field.onChange([value])}
+          defaultValue={field.value[0]}
+        >
+          {choices.map((choice) => (
+            <FormItem
+              className="flex items-center space-x-3 space-y-0"
+              key={choice.id}
+            >
+              <FormControl>
+                <RadioGroupItem value={choice.value} />
+              </FormControl>
+              <FormLabel className="font-normal">
+                {choice.value || '...'}
+              </FormLabel>
+            </FormItem>
+          ))}
+        </RadioGroup>
+      </FormControl>
+    </FormItem>
+  );
+};
